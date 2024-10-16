d0524ed05de3393ed53510d5fbb3a373af909bb7
Joshua Kuestersteffen <jkuester@kuester7.com>
2024-10-15 21:51:44 -0500

    Fix replicate impl to properly cancel the changes feed

--- dist/commands/db/replicate.js
+++ dist/commands/db/replicate.js
@@ -15,17 +15,15 @@ const printActiveTasks = (id) => active_tasks_1.CouchActiveTasksService
     .pipe(effect_1.Effect.map(effect_1.Array.findFirst(isRepTask(id))), effect_1.Effect.map(effect_1.Option.map(task => logReplicationMessage
     .pipe(effect_1.Effect.andThen(effect_1.Console.log(`Replicating docs: ${task.docs_written.toString()}`))))), effect_1.Effect.flatMap(effect_1.Option.getOrElse(() => effect_1.Effect.void)));
 const pollActiveTasks = (id) => effect_1.Effect.repeat(printActiveTasks(id), effect_1.Schedule.spaced(1000));
-const waitForCompletedRep = (changesFeed) => effect_1.Effect.async((callBack) => void changesFeed
-    .on('error', err => callBack(effect_1.Console.log(`Replication failed: ${JSON.stringify(err)}`)))
-    .on('complete', resp => callBack(effect_1.Console.log(`Could not follow replication doc changes feed: ${JSON.stringify(resp)}`)))
+const waitForCompletedRep = (changesFeed) => effect_1.Effect.async((resume) => void changesFeed
+    .on('error', err => resume(effect_1.Console.log(`Replication failed: ${JSON.stringify(err)}`)))
+    .on('complete', resp => resume(effect_1.Console.log(`Could not follow replication doc changes feed: ${JSON.stringify(resp)}`)))
     .on('change', ({ doc }) => effect_1.Option
     .fromNullable(doc)
-    .pipe(effect_1.Option.flatMap(effect_1.Option.liftPredicate(doc => doc._replication_state === 'completed')), effect_1.Option.map(() => callBack(effect_1.Console.clear.pipe(effect_1.Effect.tap(effect_1.Console.log('Replication complete'))))), effect_1.Option.getOrElse(() => callBack(effect_1.Console.log(`Replication failed: ${JSON.stringify(doc)}`))))));
+    .pipe(effect_1.Option.flatMap(effect_1.Option.liftPredicate(doc => doc._replication_state === 'completed')), effect_1.Option.map(() => effect_1.Console.clear.pipe(effect_1.Effect.tap(effect_1.Console.log('Replication complete')))), effect_1.Option.getOrElse(() => effect_1.Console.log(`Replication failed: ${JSON.stringify(doc)}`)), effect_1.Effect.tap(() => changesFeed.cancel()), resume)));
 const watchReplication = ({ id }) => replicate_1.ReplicateService
     .watch(id)
-    .pipe(effect_1.Effect.map(waitForCompletedRep), effect_1.Effect.tap(wait => effect_1.Effect
-    .fork(pollActiveTasks(id))
-    .pipe(effect_1.Effect.flatMap(fiber => wait.pipe(effect_1.Effect.andThen(effect_1.Fiber.interrupt(fiber)))))));
+    .pipe(effect_1.Effect.flatMap(waitForCompletedRep), effect_1.Effect.race(pollActiveTasks(id)));
 const follow = cli_1.Options
     .boolean('follow')
     .pipe(cli_1.Options.withAlias('f'), cli_1.Options.withDescription('After triggering replication, wait for job to complete.'), cli_1.Options.withDefault(false));
@@ -35,8 +33,7 @@ const source = cli_1.Args
 const target = cli_1.Args
     .text({ name: 'target' })
     .pipe(cli_1.Args.withDescription('The target database name.'));
-// TODO Following is broken!  Not getting the response from the changes feed now!
 exports.replicate = cli_1.Command
-    .make('replicate', { follow, source, target }, ({ follow, source, target }) => index_1.initializeUrl.pipe(effect_1.Effect.tap(logReplicationMessage), effect_1.Effect.andThen(replicate_1.ReplicateService.replicate(source, target)), effect_1.Effect.map(resp => effect_1.Option.liftPredicate(resp, () => follow)), effect_1.Effect.map(effect_1.Option.map(watchReplication)), effect_1.Effect.tap(effect_1.Option.getOrElse(() => effect_1.Console.clear.pipe(effect_1.Effect.andThen(effect_1.Console.log('Replication started. Watch the active tasks for progress: chtx active-tasks')))))))
+    .make('replicate', { follow, source, target }, ({ follow, source, target }) => index_1.initializeUrl.pipe(effect_1.Effect.tap(logReplicationMessage), effect_1.Effect.andThen(replicate_1.ReplicateService.replicate(source, target)), effect_1.Effect.map(resp => effect_1.Option.liftPredicate(resp, () => follow)), effect_1.Effect.map(effect_1.Option.map(watchReplication)), effect_1.Effect.flatMap(effect_1.Option.getOrElse(() => effect_1.Console.clear.pipe(effect_1.Effect.andThen(effect_1.Console.log('Replication started. Watch the active tasks for progress: chtx active-tasks')))))))
     .pipe(cli_1.Command.withDescription(`Triggers a one-time replication from the source to the target database.`));
 //# sourceMappingURL=replicate.js.map
--- src/commands/db/replicate.ts
+++ src/commands/db/replicate.ts
@@ -1,5 +1,5 @@
 import { Args, Command, Options } from '@effect/cli';
-import { Array, Console, Effect, Fiber, Option, Schedule } from 'effect';
+import { Array, Console, Effect, Option, Schedule } from 'effect';
 import { initializeUrl } from '../../index';
 import { ReplicateService, ReplicationDoc } from '../../services/replicate';
 import { CouchActiveTask, CouchActiveTasksService } from '../../services/couch/active-tasks';
@@ -25,28 +25,26 @@ const pollActiveTasks = (id: string) => Effect.repeat(printActiveTasks(id), Sche
 
 const waitForCompletedRep = (
   changesFeed: PouchDB.Core.Changes<ReplicationDoc>
-) => Effect.async((callBack) => void changesFeed
-  .on('error', err => callBack(Console.log(`Replication failed: ${JSON.stringify(err)}`)))
-  .on('complete', resp => callBack(
+): Effect.Effect<void> => Effect.async((resume) => void changesFeed
+  .on('error', err => resume(Console.log(`Replication failed: ${JSON.stringify(err)}`)))
+  .on('complete', resp => resume(
     Console.log(`Could not follow replication doc changes feed: ${JSON.stringify(resp)}`)
   ))
   .on('change', ({ doc }) => Option
     .fromNullable(doc)
     .pipe(
       Option.flatMap(Option.liftPredicate(doc => doc._replication_state === 'completed')),
-      Option.map(() => callBack(Console.clear.pipe(Effect.tap(Console.log('Replication complete'))))),
-      Option.getOrElse(() => callBack(Console.log(`Replication failed: ${JSON.stringify(doc)}`))),
+      Option.map(() => Console.clear.pipe(Effect.tap(Console.log('Replication complete')))),
+      Option.getOrElse(() => Console.log(`Replication failed: ${JSON.stringify(doc)}`)),
+      Effect.tap(() => changesFeed.cancel()),
+      resume,
     )));
 
 const watchReplication = ({ id }: PouchDB.Core.Response) => ReplicateService
   .watch(id)
   .pipe(
-    Effect.map(waitForCompletedRep),
-    Effect.tap(wait => Effect
-      .fork(pollActiveTasks(id))
-      .pipe(Effect.flatMap(fiber => wait.pipe(
-        Effect.andThen(Fiber.interrupt(fiber))
-      ))))
+    Effect.flatMap(waitForCompletedRep),
+    Effect.race(pollActiveTasks(id)),
   );
 
 const follow = Options
@@ -63,15 +61,13 @@ const target = Args
   .text({ name: 'target' })
   .pipe(Args.withDescription('The target database name.'));
 
-
-// TODO Following is broken!  Not getting the response from the changes feed now!
 export const replicate = Command
   .make('replicate', { follow, source, target }, ({ follow, source, target }) => initializeUrl.pipe(
     Effect.tap(logReplicationMessage),
     Effect.andThen(ReplicateService.replicate(source, target)),
     Effect.map(resp => Option.liftPredicate(resp, () => follow)),
     Effect.map(Option.map(watchReplication)),
-    Effect.tap(Option.getOrElse(() => Console.clear.pipe(
+    Effect.flatMap(Option.getOrElse(() => Console.clear.pipe(
       Effect.andThen(Console.log('Replication started. Watch the active tasks for progress: chtx active-tasks'))
     ))),
   ))
