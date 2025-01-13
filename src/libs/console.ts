e9e0550f135704892aba43424666fee62bf776bb
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-01-13 13:15:26 -0600

    chore: upgrade knip and fix linting

--- src/libs/console.ts
+++ src/libs/console.ts
@@ -5,7 +5,7 @@ const ANSI_CODES = {
   green: '\x1b[32m',
   blue: '\x1b[34m'
 };
-export type AnsiColor = keyof typeof ANSI_CODES;
+type AnsiColor = keyof typeof ANSI_CODES;
 const resetCode = '\x1b[0m';
 
 export const color = (color: AnsiColor) => (text: string): string => `${ANSI_CODES[color]}${text}${resetCode}`;
