d1d64cd31cb9cef66b4ed833e56142536c254f00
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-03-28 08:03:06 -0500

    chore: upgrade dependencies (#103)

--- dist/services/upgrade.d.ts
+++ dist/services/upgrade.d.ts
@@ -4,16 +4,16 @@ import { Schema, Stream } from 'effect';
 import { ChtClientService } from './cht-client.js';
 declare const UpgradeLog_base: Schema.Class<UpgradeLog, {
     _id: typeof Schema.String;
-    state: Schema.Schema<string, string, never>;
+    state: Schema.SchemaClass<string, string, never>;
     state_history: Schema.Array$<Schema.Struct<{
-        state: Schema.Schema<string, string, never>;
+        state: Schema.SchemaClass<string, string, never>;
         date: typeof Schema.Number;
     }>>;
 }, Schema.Struct.Encoded<{
     _id: typeof Schema.String;
-    state: Schema.Schema<string, string, never>;
+    state: Schema.SchemaClass<string, string, never>;
     state_history: Schema.Array$<Schema.Struct<{
-        state: Schema.Schema<string, string, never>;
+        state: Schema.SchemaClass<string, string, never>;
         date: typeof Schema.Number;
     }>>;
 }>, never, {
