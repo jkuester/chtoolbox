0e66693838a1f1c5a1922cbbba23b57e816ab5cf
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-08-19 16:09:00 -0500

    Add support for updating the sentinel backlog

--- test/libs/console.spec.ts
+++ test/libs/console.spec.ts
@@ -2,22 +2,22 @@ import { describe, it } from 'mocha';
 import { Console, Effect, Logger, LogLevel, TestContext } from 'effect';
 import { expect } from 'chai';
 import sinon from 'sinon';
-import { clearConsole, clearThen, debugLoggingEnabled, logJson } from '../../src/libs/console.ts';
+import { clearConsole, clearThen, debugLoggingEnabled, logJson, color } from '../../src/libs/console.ts';
 
 describe('Console libs', () => {
   const run = (test:  Effect.Effect<void, Error>) => async () => {
     await Effect.runPromise(test.pipe(Effect.provide(TestContext.TestContext)));
   };
 
-  // [
-  //   ['red', '\x1b[31m'],
-  //   ['green', '\x1b[32m'],
-  //   ['blue', '\x1b[34m']
-  // ].forEach(([ansiColor, expectedCode]) => {
-  //   it(`color(${ansiColor})`, () => {
-  //     expect(color(ansiColor as 'red' | 'green' | 'blue')('hello')).to.equal(`${expectedCode}hello\x1b[0m`);
-  //   });
-  // });
+  ([
+    ['red', '\x1b[31m'],
+    ['green', '\x1b[32m'],
+    ['blue', '\x1b[34m']
+  ] as [string, string][]).forEach(([ansiColor, expectedCode]) => {
+    it(`color(${ansiColor})`, () => {
+      expect(color(ansiColor as 'red' | 'green' | 'blue')('hello')).to.equal(`${expectedCode}hello\x1b[0m`);
+    });
+  });
 
   describe('debugLoggingEnabled', () => {
     it('returns true when DEBUG logging is enabled', run(Effect.gen(function* () {
