497edace407403471ff9d5108be22ad0d0f68c57
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-08-22 08:06:32 -0500

    Clean up tsconfig and fix linting

--- bin/index.js
+++ bin/index.js
@@ -5,9 +5,7 @@ import { resolve, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 /**
- * This file is a shim for running the TS directly as a bin script. It automatically applies the
- * --experimental-strip-types and --no-warnings=ExperimentalWarning (to suppress the warnings) flags to the node
- * process.
+ * This file is a shim for running the TS directly as a bin script.
  */
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const entry = resolve(__dirname, '..', 'src', 'index.ts');
