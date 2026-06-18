8e4e20cf51cf3e7a37c51a600fa4057b2e67f0a5
Joshua Kuestersteffen <jkuester@kuester7.com>
2026-06-18 13:36:59 -0500

    Support formatting multiple files and directories

--- src/commands/form/format.ts
+++ src/commands/form/format.ts
@@ -1,23 +1,56 @@
-import { Args, Command } from '@effect/cli';
+import { FileSystem } from '@effect/platform';
+import { Args, Command, Options } from '@effect/cli';
 import { Array, Effect, pipe } from 'effect';
 import { FormService } from '../../services/form.ts';
 
-const formatFile = (filePath: string) => pipe(
-  FormService.formatFile(filePath)
+const formatFile = (filePath: string) => FormService.formatFile(filePath);
+
+const getDirectoryFiles = (directory: string) => FileSystem.FileSystem.pipe(
+  Effect.flatMap(fs => fs.readDirectory(directory)),
+  Effect.map(Array.filter(fileName => fileName.endsWith('.xlsx'))),
+  Effect.map(Array.map(fileName => `${directory}/${fileName}`)),
+);
+
+const getFilesToFormat = (files: string[], directories: string[]) => pipe(
+  directories,
+  Array.map(getDirectoryFiles),
+  Effect.all,
+  Effect.map(Array.flatten),
+  Effect.map(Array.appendAll(files)),
+);
+
+const assertArgs = (files: string[], directories: string[]) => pipe(
+  Effect.fail(new Error('Must provide either `file` args or the --directory option.')),
+  Effect.when(() => Array.isEmptyArray(files) && Array.isEmptyArray(directories)),
 );
 
 const files = Args
-  .text({ name: 'file' })
+  .file({
+    name: 'file',
+    exists: 'yes'
+  })
   .pipe(
     Args.withDescription('Path to .xlsx file(s) to format'),
-    Args.atLeast(1),
+    Args.repeated,
+  );
+
+const directories = Options
+  .directory('directory', { exists: 'yes' })
+  .pipe(
+    Options.withAlias('d'),
+    Options.withDescription(
+      'A local directory containing the forms to format. The directory should directly contain the .xlsx '
+      + 'files. May be repeated to format multiple directories, and may be combined with `file` args.'
+    ),
+    Options.repeated,
   );
 
 export const format = Command
-  .make('format', { files }, Effect.fn(({ files }) => pipe(
-    files,
-    Array.map(formatFile),
-    Effect.all,
+  .make('format', { files, directories }, Effect.fn(({ files, directories }) => pipe(
+    assertArgs(files, directories),
+    Effect.andThen(getFilesToFormat(files, directories)),
+    Effect.map(Array.map(formatFile)),
+    Effect.flatMap(Effect.all),
     Effect.asVoid,
   )))
   .pipe(Command.withDescription('Apply conditional formatting to .xlsx form file(s).'));
